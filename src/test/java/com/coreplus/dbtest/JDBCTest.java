729dce83b931bdaa9b6f7da86208a72fa0aeb04c
COREPLUS-USER <COREPLUS-USER@DESKTOP-EGAJ7UF>
2020-01-15 16:34:25 +0900

    test

---
+++ src/test/java/com/coreplus/dbtest/JDBCTest.java
@@ -0,0 +1,32 @@
+package com.coreplus.dbtest;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTest {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try(Connection con= DriverManager.getConnection(
+				"jdbc:oracle:thin:@192.168.0.53:1521:orcl","test","test")){
+			log.info(con);
+		} catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
